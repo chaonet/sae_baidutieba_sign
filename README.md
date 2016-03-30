9678f877c5a3e9e5895f1b16ede26acca5ab0a5e
chao xu <chao@chaodeMacBook-Pro.local>
2016-03-30 12:29:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,95 @@
+## 百度贴吧自动签到_SAE 版本
+
+## 功能
+
+之前做了一个[百度贴吧自动签到](https://github.com/chaonet/baidu_tieba_auto_sign)，但每天手动重复运行也麻烦，于是想到用`SAE`。
+
+部署到 SAE，使用`计划任务服务`，每天 0 点自动运行，可以参考[Cron](http://www.sinacloud.com/doc/sae/python/cron.html)
+
+## 开发环境
+
+```
+Python 2.7.9
+Mac os 10.10.5
+virtualenv 13.1.2
+pip 8.0.2
+```
+
+## 环境配置
+
+```
+# 下载源码
+git clone git@github.com:chaonet/sae_baidutieba_sign.git
+# 或者通过 HTTPS 下载:
+git clone https://github.com/chaonet/sae_baidutieba_sign.git
+
+cd sae_baidutieba_sign
+
+# 安装依赖
+virtualenv env/
+. ./env/bin/activate
+pip install -r requirements.txt
+```
+
+## 准备
+
+注册[新浪云账号](http://www.sinacloud.com/home/index/faq_detail/doc_id/19.html)，需要通过微博账号登陆，所以，还要注册一个微博……
+
+创建一个`云应用 SAE`，填写`二级域名`，也就是`App name`，输入`应用名称`，运行环境选择`Python 2.7`，代码管理方式使用`git`而不是`svn`
+
+## 使用
+
+- 设置账号、密码
+
+创建`conf.ini`文件，用于保存 百度 账号、密码，以便脚本运行时读取
+
+内容：
+
+```
+[main]
+username=账号
+password=密码
+```
+
+或者
+
+在系统的环境变量中设置，方法略……
+
+- 生成`cookie.txt`文件
+
+`python get_cookie_file.py`
+
+- 部署到 SAE
+
+在你应用的git代码目录里，添加一个新的git远程仓库 sae
+
+```
+$ git remote add sae https://git.sinacloud.com/应用的二级域名(App name)
+```
+
+编辑代码并将代码部署到 `sae` 的版本1。
+
+```
+$ git add .
+$ git commit -am "make it better"
+$ git push sae master:1
+```
+
+## change logs
+
+将应用代码放入`index.wsgi`，并在`config.yaml`中完成应用信息配置和`cron`任务
+
+将第三方依赖导入`vendor`目录
+
+安装本地 sae 测试环境`sae-python-dev-guide`
+
+本地测试运行正常，但部署到`SAE`后，获取`token`失败
+
+经过测试，在`SAE`上运行，可以打开`http://www.baidu.com/`，无法获取`https://passport.baidu.com/v2/api/?getapi&tpl=pp&apiver=v3`，返回的是`http://www.baidu.com/search/error.html`
+
+于是，考虑先本地获取并保存`cookie`到应用目录，然后在线上环境加载`cookie`。
+
+成功
+
+学了一遍 python 爬虫 的知识
+
---
+++ get_cookie_file.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os, sys
+
+import urllib
+import urllib2
+import cookielib
+import re
+import hashlib
+import json
+import threading
+import platform
+import time
+
+import ConfigParser
+
+defaultencoding = 'utf-8'
+
+if sys.getdefaultencoding() != defaultencoding:
+    reload(sys)
+    sys.setdefaultencoding(defaultencoding)
+
+system_env = True
+
+user = ""
+password = ""
+
+cookie_get = ""
+BDUSS = ""
+
+if os.path.exists('conf.ini'):
+    cf = ConfigParser.ConfigParser()
+    cf.read('conf.ini')
+    user = cf.get('main', 'username')
+    password = cf.get('main', 'password')
+else:
+    user = os.environ.get("username")
+    password = os.environ.get("password")
+
+TOKEN = ""
+TOKEN_URL = "http://passport.baidu.com/v2/api/?getapi&tpl=pp&apiver=v3"
+
+INDEX_URL = "http://www.baidu.com/"
+LOGIN_URL = "https://passport.baidu.com/v2/api/?login"
+
+loginHeaders = {
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding":"gzip,deflate,sdch",
+    "Accept-Language":"en-US,en;q=0.8,zh;q=0.6",
+    "Host":"passport.baidu.com",
+    'Upgrade-Insecure-Requests':'1',
+    "Origin":"http://www.baidu.com",
+    "Referer":"http://www.baidu.com/",
+    'User-agent':'Mozilla/5.0 (SymbianOS/9.3; Series60/3.2 NokiaE72-1/021.021; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/525 (KHTML, like Gecko) Version/3.0 BrowserNG/7.1.16352'
+}
+# "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36"
+
+
+bdData = {
+    "staticpage":"https://passport.baidu.com/static/passpc-account/html/V3Jump.html",
+    "token":"",
+    "tpl":"pp",
+    "username":user,
+    "password":password,
+    "loginmerge":"true",
+    "mem_pass":"on",
+    "logintype":"basicLogin",
+    "logLoginType":"pc_loginBasic",
+}
+
+
+def get_cookie_and_bduss():
+    global cookie_get,BDUSS
+    success = False
+    filename = 'cookie.txt'
+    cookie = cookielib.MozillaCookieJar(filename)
+
+    while not success:
+        # cookie = cookielib.CookieJar()
+        # cookie = cookielib.MozillaCookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+        opener.addheaders = [('User-agent', 'Mozilla/5.0 (SymbianOS/9.3; Series60/3.2 NokiaE72-1/021.021; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/525 (KHTML, like Gecko) Version/3.0 BrowserNG/7.1.16352')]
+
+        #get TOKEN
+        print("Loading...正在获取token:")
+        #try:
+        data = opener.open(INDEX_URL).read() # 正常获取
+        print data, 0
+        data = opener.open(TOKEN_URL).read() # 获取的是错误页面`http://www.baidu.com/search/error.html`
+        print data,1
+        
+        print type(data)
+        "<type 'str'>"
+        data = eval(data)
+        print data,2
+        print data["data"]["token"]
+        TOKEN = data["data"]["token"]
+        
+        # print re.compile("\"token\"\s+:\s+\"(\w+)\"").findall(str(data))
+        # TOKEN = re.compile("\"token\"\s+:\s+\"(\w+)\"").findall(str(data))[0]
+        bdData["token"] = TOKEN
+        print("token获取成功:" + TOKEN)
+        #except:
+            #print("网络问题 无法为您获取token,正在退出...")
+            #return
+        #login
+        data = urllib.urlencode(bdData)
+        request = urllib2.Request(LOGIN_URL, headers=loginHeaders, data=data)
+        # request = urllib2.Request(LOGIN_URL, data=data)
+        # request = urllib2.Request(LOGIN_URL, headers=loginHeaders, data=urllib.urlencode(bdData).encode('utf-8'))
+        
+        print "正在为 %s 进行登录操作" % user
+        #try:
+        # result = opener.open(request).read()
+        result = opener.open(request)
+        print result,3
+        # <addinfourl at 4354171608 whose fp = <socket._fileobject object at 0x1038540d0>> 3
+        # result = json.loads(opener.open("http://tieba.baidu.com/f/user/json_userinfo").read().decode("utf-8"))
+        # result = json.loads(opener.open("http://tieba.baidu.com/f/user/json_userinfo").read().decode("ISO-8859-1"))
+        cookiesToCheck_dir = {'BDUSS':'', 'PTOKEN':'', 'STOKEN':'', 'SAVEUSERID':''}
+        cookiesToCheck_list = ['BDUSS', 'PTOKEN', 'STOKEN', 'SAVEUSERID']
+        print cookie, 4
+        success = True
+        for ck in cookiesToCheck_list:
+            for ck_1 in cookie:
+                if ck_1.name == ck:
+                    cookiesToCheck_dir[ck] = True
+        for ck in cookiesToCheck_list:
+            if not cookiesToCheck_dir[ck]:
+                success = False
+
+
+    print user + "登录成功!"
+    cookie.save(ignore_discard=True, ignore_expires=True)
+        # print cookie
+    for ck in cookie:
+        if ck.name == 'BDUSS':
+            BDUSS = ck.value 
+            # print BDUSS
+            cookie_get = cookie
+    print cookie_get
+    print BDUSS
+    # return cookie,BDUSS
+
+get_cookie_and_bduss()
+
